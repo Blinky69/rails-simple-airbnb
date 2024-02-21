e3aeb655447d1f88ec02459f564945a71187e6ef
Jason Roche <jason.p.roche@hotmail.com>
2024-02-21 17:16:52 +0800

    Almost

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,20 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'faker'
+
+puts "Destroying all data"
+Flat.destroy_all
+puts "All data destroyed"
+puts "Creating data"
+options = %w[Villa Flats House Townhouse]
+10.times do
+  Flat.create(
+    name: "#{Faker::Company.name} #{options.sample} in #{Faker::Address.city}",
+    address: Faker::Address.street_address,
+    description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+    price_per_night: rand(40..100),
+    number_of_guests: rand(1..6)
+  )
+end
+puts "Fake data created"
